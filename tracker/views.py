e7d96c2131cda0b41677758de0706227597585d9
bright-prajwal <honprajwal9@gmail.com>
2025-09-18 14:49:24 +0530

    UI logic and BackEnd(first) Done

--- tracker/views.py
+++ tracker/views.py
@@ -1,8 +1,107 @@
-from django.shortcuts import render
+# from django.shortcuts import render,redirect
+# from .models import TrackingHistory, CurrentBalance
+# # Create your views here.
 
-# Create your views here.
+# # from django.shortcuts import render
 
-# from django.shortcuts import render
+# def index(request):
+#     if request.method=="POST":
+#         description=request.POST.get('description')
+#         amount=request.POST.get('amount')
+
+#         current_balance=current_balance.objects.get()
+
+#         print(description,amount)
+#         return redirect('/')
+#     return render(request, 'index.html')
+
+
+from django.shortcuts import render,redirect
+from .models import *
+from django.contrib import messages
+from django.db.models import Sum
+
+
+# def index(request):
+#     if request.method == "POST":
+#         description = request.POST.get('description')
+#         amount = request.POST.get('amount')
+
+#         current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+#         expense_type = "CREDIT"
+#         if float(amount) < 0:
+#             expense_type = "DEBIT"
+
+#         if float(amount) == 0:
+#             messages.success(request, "Amount cannot be zero") 
+#             return redirect('/')
+
+#         tracking_history = TrackingHistory.objects.create(amount = amount,
+#             expense_type = expense_type,
+#             current_balance = current_balance,
+#             description = description)
+#         current_balance.current_balance += float(tracking_history.amount)
+#         current_balance.save()
+#         # print(description,amount)
+#         return redirect('/')
+    
+#     current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+#     context={'transactions':TrackingHistory.objects.all(), 'current_balance':current_balance}
+#     return render(request, 'index.html',context)
+
+#in views.py
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "POST":
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+
+        current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+        expense_type = "CREDIT"
+        if float(amount) < 0:
+            expense_type = "DEBIT"
+
+        if float(amount) == 0:
+            messages.success(request, "Amount cannot be zero") 
+            return redirect('/')
+
+        tracking_history = TrackingHistory.objects.create(amount = amount,
+            expense_type = expense_type,
+            current_balance = current_balance,
+            description = description)
+        current_balance.current_balance += float(tracking_history.amount)
+        current_balance.save()
+        print(description,amount)
+        return redirect('/')
+      # updated code
+    current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+    income = 0
+    expense = 0
+    
+    for tracking_history in TrackingHistory.objects.all():
+        if tracking_history.expense_type == "CREDIT":
+            income += tracking_history.amount
+        else:
+            expense += tracking_history.amount
+
+    context = {'income' : income,
+                'expense' : expense , 
+                'transactions' : TrackingHistory.objects.all() , 'current_balance' : current_balance}
+    return render(request, 'index.html' , context)
+
+
+# in views.py
+def delete_transaction(request, id):
+    tracking_history = TrackingHistory.objects.filter(id = id)
+
+    if tracking_history.exists():
+        current_balance, _ = CurrentBalance.objects.get_or_create(id = 1)
+        tracking_history = tracking_history[0]
+        
+        current_balance.current_balance = current_balance.current_balance - tracking_history.amount
+
+        current_balance.save()
+
+
+    tracking_history.delete()
+    return redirect('/')
